872bb62240d503839a1f1a71e339468b994d1080
akhilesh <akhileshkunnath17@gmail.com>
2024-08-14 15:06:00 +0530

    user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -20,7 +20,7 @@ const DashSidebar = () => {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup>
-                <Link to={"/dashboard?tab=profile"}><Sidebar.Item active ={tab === "profile"} icon ={HiUser} label={"user"} labelColor = {"dark"}>
+                <Link to={"/dashboard?tab=profile"}><Sidebar.Item active ={tab === "profile"} icon ={HiUser} label={"user"} labelColor = {"dark"} as="div">
                     profile
                 </Sidebar.Item></Link>
                 <Sidebar.Item  icon ={HiArrowSmRight} >
