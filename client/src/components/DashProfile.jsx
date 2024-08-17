3c7f91cfd3f33fb679be5590cd53449ff2e9593f
akhilesh <akhileshkunnath17@gmail.com>
2024-08-17 11:28:25 +0530

    create create post UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -9,9 +9,10 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateSuccess,updateFailure,deleteUserFailure,deleteUserStart,deleteUserSuccess,signoutSuccess } from "../Redux/user/userSlice"
 import { useDispatch } from "react-redux"
 import {HiOutlineExclamationCircle} from "react-icons/hi"
+import {Link} from "react-router-dom"
 
 const DashProfile = () => {
-    const {currentUser, error} = useSelector(state=>state.user)
+    const {currentUser, error , loading} = useSelector(state=>state.user)
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress,setImageFileUploadProgress] = useState(null)
@@ -206,10 +207,20 @@ const DashProfile = () => {
        <TextInput  type="text" id="username" placeholder="username" defaultValue={currentUser.username} onChange={handleChange} />
        <TextInput type="email" id="email" placeholder="email" defaultValue={currentUser.email} onChange={handleChange}/>
        <TextInput type="password" id="password" placeholder="password"  onChange={handleChange}/>
-       <Button type="submit" className="" gradientDuoTone={"purpleToBlue"} outline>
-        Update
+       <Button type="submit" className="" gradientDuoTone={"purpleToBlue"} outline disabled={loading || imageFileUploading}>
+        {loading ? "Loading... " : "Update"}
 
        </Button>
+       {
+        currentUser.isAdmin && (
+           <Link to="/create-post">
+            <Button type="button" gradientDuoTone={"purpleToPink"} className="w-full">
+                create a post
+
+            </Button>
+           </Link>
+        )
+       }
        </form>
        <div className="text-red-500 flex justify-between mt-5">
         <span onClick={()=>{setShowModal(true)}}>Delete Account</span>
