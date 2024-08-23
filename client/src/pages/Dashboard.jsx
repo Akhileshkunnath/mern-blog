cabeee82fe492dc5abd169f814b1b12ed1f64ac2
akhilesh <akhileshkunnath17@gmail.com>
2024-08-23 16:40:48 +0530

    show user to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import { DashPosts } from "../components/DashPosts";
+import { DashUsers} from "../components/DashUsers";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -36,6 +37,9 @@ const Dashboard = () => {
 
       }
       
+      {
+        tab === "users" && <DashUsers/>
+      }
       
       
      </div>
