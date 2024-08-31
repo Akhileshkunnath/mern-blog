f3ac6bd3017c4e693cbe47d775ec0fc750ff6cfe
akhilesh <akhileshkunnath17@gmail.com>
2024-08-31 15:34:40 +0530

    final change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
   return (
     <div><div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
       <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-      <p className='text-gray-500 text-xs sm:text-sm'>This is a place where you'll find a variety of articles and tutorials on topics related to web development, software engineering and sports :)
+      <p className='text-gray-500 text-xs sm:text-sm'>This is a place where you'll find a variety of articles and tutorials on topics related to web development, software engineering and others :)
         </p>
         <Link to="/search" className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all post</Link>
         
